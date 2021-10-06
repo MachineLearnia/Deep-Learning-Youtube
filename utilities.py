432c70f17bb5f3f11ff43c682994853046c3f455
MachineLearnia <53819539+MachineLearnia@users.noreply.github.com>
2021-10-06 19:42:17 +0100

    Add files via upload

---
+++ utilities.py
@@ -0,0 +1,14 @@
+import h5py
+import numpy as np
+
+
+def load_data():
+    train_dataset = h5py.File('datasets/trainset.hdf5', "r")
+    X_train = np.array(train_dataset["X_train"][:]) # your train set features
+    y_train = np.array(train_dataset["Y_train"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/testset.hdf5', "r")
+    X_test = np.array(test_dataset["X_test"][:]) # your train set features
+    y_test = np.array(test_dataset["Y_test"][:]) # your train set labels
+    
+    return X_train, y_train, X_test, y_test
